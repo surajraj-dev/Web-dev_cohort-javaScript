eabd79ed96ca6a04a3318f5e6a8119e4085e538d
Suraj Raj <srjdev2003@gmail.com>
2025-02-10 13:10:16 +0530

    doWhileLoop.js file added

---
+++ Day_01/BasicsLoops/doWhileLoop.js
@@ -0,0 +1,28 @@
+/*
+        🔥 do...while Loop in JavaScript  🔥
+        📌 Definition:
+        do...while loop ek baar minimum execute hota hi hai, chahe condition false ho ya true.
+        :-> phle kro fir sochega 😂😂😂😂
+
+*/
+
+            let i = 1;
+
+            do {
+            console.log(i);  // Print karega
+            i++;
+            } 
+            while (i <= 5);
+
+
+
+// ***********************************************************************************
+/*                using input = prompt
+        🔥 Best 5 Practice Questions for do...while Loop 🔥
+            1️⃣ Print numbers from 1 to 10.
+            2️⃣ Print reverse counting from 10 to 1.
+            3️⃣ Print even numbers from 2 to 20.
+            4️⃣ Print the multiplication table of any number.
+            5️⃣ Take user input and keep printing until they enter "stop" (using readline).
+
+*/
